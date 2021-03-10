51b701278593c7e43ebf256eda5054ab1ebe5df2
Diretnan Domnan <diretnandomnan@gmail.com>
2021-03-10 02:36:22 +0100

    nginx setup for mythra

--- terraform/gcp.tf
+++ terraform/gcp.tf
@@ -8,6 +8,8 @@ resource "google_compute_instance" "vm_instance" {
   count        = 1
   name         = "${var.mythra_project}-ce"
   machine_type = "f1-micro"
+  
+  tags = ["http-server", "https-server"]
 
   boot_disk {
     initialize_params {
@@ -27,6 +29,17 @@ resource "google_compute_instance" "vm_instance" {
   }
 }
 
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80","443"]
+  }
+
+}
+
 resource null_resource "config-mythra-server-ansible" {
   triggers = {
     "src_hash" = data.archive_file.ansible_dir.output_sha # track changes in the ansible dir
@@ -37,7 +50,7 @@ resource null_resource "config-mythra-server-ansible" {
       ANSIBLE_HOST_KEY_CHECKING = "False"
     }
     command = <<CMD
-#      sleep 60
+      sleep 60
       ansible-playbook -u gcp --private-key ../terraform/gophie-private-key.pem  -i '${google_compute_address.static.address},' mythra.yml
     CMD
   }
