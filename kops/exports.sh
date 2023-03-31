d2fdff496ed5223109df871319e1b51b077d6174
bethel-m <bethelcletus87@gmail.com>
2023-03-31 11:15:00 +0100

    added manifests for apps

--- kops/exports.sh
+++ kops/exports.sh
@@ -6,6 +6,7 @@ export KOPS_STATE_STORE=s3://bisohns-k8s-local-state-store
 export NAME=k8s.bisohns.com
 export OIDC_STORE=s3://bisohns-k8s-local-oidc-store
 export VPC_ID=vpc-de3b14b6
+export NODE_ZONES="eu-west-2a,eu-west-2b"
 export MASTER_ZONES="eu-west-2a,eu-west-2b,eu-west-2c"
 export MASTER_SIZE="t3.medium"
 export NODE_SIZE="t3.medium"
