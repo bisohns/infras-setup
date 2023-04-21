b164cc11fa5b3d6dac25cf60e42fa784f2935f53
Mmadu Manasseh <mmadumanasseh@gmail.com>
2023-04-21 14:41:01 +0100

    Migrating to GCP

---
+++ terraform/gcp/gke.tf
@@ -0,0 +1,49 @@
+resource "google_container_cluster" "primary" {
+  name     = "khidom-gke-01"
+  location = "us-west1-a"
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block  = "10.1.0.0/16"
+    services_ipv4_cidr_block = "172.20.0.0/20"
+  }
+
+  networking_mode = "VPC_NATIVE"
+  network         = google_compute_network.khidom_01.id
+  subnetwork      = google_compute_subnetwork.gke.id
+
+}
+
+resource "google_service_account" "node_pool" {
+  account_id   = "gke-node-pool"
+  display_name = "GKE Node pool Service Account"
+}
+
+resource "google_container_node_pool" "default" {
+  name     = "default-node-pool-01"
+  location = "us-west1-a"
+  node_locations = [
+    "us-west1-a"
+  ]
+  cluster    = google_container_cluster.primary.name
+  node_count = 2
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.node_pool.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+  lifecycle {
+    ignore_changes = [initial_node_count]
+  }
+}
---
+++ terraform/gcp/terraform.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "gcs" {
+    bucket = "khidom-terraform-state"
+    prefix = "global/terrform"
+  }
+  required_version = "~> 1.4"
+  required_providers {
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 4.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "khidom"
+  region  = "us-west1"
+}
+
+provider "google-beta" {
+  project = "khidom"
+  region  = "us-west1"
+}
